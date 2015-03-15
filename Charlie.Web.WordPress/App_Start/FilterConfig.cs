6a66cffa3dd5309e7c5f418a0170c8f7e6f88605
charliefr <hbook@qq.com>
2015-03-15 18:13:57 +0800

    insert

---
+++ Charlie.Web.WordPress/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Charlie.Web.WordPress
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Charlie.Web.WordPress/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Charlie.Web.WordPress.MvcApplication" Language="C#" %>
