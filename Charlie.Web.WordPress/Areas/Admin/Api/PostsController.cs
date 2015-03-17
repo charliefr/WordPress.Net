327fbf8c70d1984152af3f91e369df3bf488ffc3
charliefr <hbook@qq.com>
2015-03-17 14:49:29 +0800

    update

---
+++ Charlie.Web.WordPress/Areas/Admin/Api/PostsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Charlie.Web.WordPress.Data;
+using Charlie.Web.WordPress.Data.Models;
+
+namespace Charlie.Web.WordPress.Areas.Admin.Api
+{
+    public class PostsController : ApiController
+    {
+        private readonly SqlDataContext _db = new SqlDataContext();
+
+        // GET: api/Posts
+        public IQueryable<Post> GetPosts()
+        {
+            return _db.Posts;
+        }
+
+        // GET: api/Posts/5
+        [ResponseType(typeof(Post))]
+        public async Task<IHttpActionResult> GetPost(int id)
+        {
+            var post = await _db.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+        // PUT: api/Posts/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPost(int id, Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != post.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(post).State = EntityState.Modified;
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Posts
+        [ResponseType(typeof(Post))]
+        public async Task<IHttpActionResult> PostPost(Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.Posts.Add(post);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = post.Id }, post);
+        }
+
+        // DELETE: api/Posts/5
+        [ResponseType(typeof(Post))]
+        public async Task<IHttpActionResult> DeletePost(int id)
+        {
+            var post = await _db.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            _db.Posts.Remove(post);
+            await _db.SaveChangesAsync();
+
+            return Ok(post);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PostExists(int id)
+        {
+            return _db.Posts.Count(e => e.Id == id) > 0;
+        }
+
+      
+    }
+}
